d5292093c42c0a48289823e345fa73c93fb2f717
Peter Griess <pg@std.in>
2022-08-08 09:15:05 -0500

    Add support for logging
    - Use loglevel NPM library
    - Add -v, -q options to cli.js driver
    - Remove all console.* invocations
    
    Fixes #1

--- src/cli.js
+++ src/cli.js
@@ -1,45 +1,75 @@
 #!/usr/bin/env node
 "use strict";
 
-import { ArgumentParser } from "argparse";
+import { exit } from "node:process"
+import { inspect } from "node:util"
+import { ArgumentParser } from "argparse"
+import log from "loglevel" // CommonJS, not ES6 module
 import { createSession, getBatteryInfo, setBatteryInfo } from "./enphase.js"
 import { nextState, State, stateFromBatteryInfo } from "./state.js"
-import { exit } from "node:process"
 
 const ap = ArgumentParser({
     "description": "The command line interface to Picogrid."
 });
 ap.add_argument(
+    "-p",
+    {
+        dest: "password",
+        metavar: "<password>",
+        type: "str",
+        help: "Enphase password"
+    },
+)
+ap.add_argument(
+    "-q",
+    {
+        dest: "quiet",
+        action: "store_true",
+        default: false,
+        help: "silence all logging regardless of verbosity"
+    }
+)
+ap.add_argument(
     "-u",
     {
+        dest: "username",
         metavar: "<username>",
         type: "str",
         help: "Enphase username"
     },
 )
 ap.add_argument(
-    "-p",
+    "-v",
     {
-        metavar: "<password>",
-        type: "str",
-        help: "Enphase password"
-    },
+        dest: "verbosity",
+        action: "count",
+        default: 0,
+        help: "increase logging verbosity; can be used multiple times"
+    }
 )
 
 const args = ap.parse_args();
-const session = await createSession(args.u, args.p)
+
+// Configure logging
+log.setDefaultLevel(
+    (args.quiet) ?
+        log.levels.SILENT :
+        Math.max(log.levels.TRACE, log.levels.ERROR - args.verbosity)
+)
+
+const session = await createSession(args.username, args.password)
 if (!session) {
-    console.error("Failed to log in")
+    log.error("Failed to log in!")
     exit(1)
 }
 
 const bi = await getBatteryInfo(session)
-console.log(bi)
+log.debug(`battery_info=${inspect(bi)}`)
 const cs = stateFromBatteryInfo(bi)
 const ns = nextState(new Date())
 
 if (ns === cs) {
-    console.log("Current state un-changed; doing nothing")
+    log.info("Current state un-changed; doing nothing")
     exit(0)
 }
 
@@ -53,6 +83,6 @@ if (nbi === undefined) {
     throw new Error(`Unexpected state: ${ns}`)
 }
 
-console.log(`Changing current state to ${ns.toString()}`)
+log.info(`Changing current state to ${ns.toString()}`)
 
 await setBatteryInfo(session, nbi)
