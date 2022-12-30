ae07dd4418438c6b001e3a5b11961c916b158add
Peter Griess <pg@std.in>
2022-12-30 13:12:29 -0600

    Misc small whitespace tweaks

--- src/cli.js
+++ src/cli.js
@@ -5,9 +5,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -113,6 +113,7 @@ if (!session) {
 
 const bi = await getBatteryInfo(session, abortController.signal)
 log.debug(`battery_info=${inspect(bi)}`)
+
 const cs = stateFromBatteryInfo(bi)
 const ns = nextState(DateTime.now())
 
