e325459871a65abde72c523fdba72ba2cf8b3360
Peter Griess <pg@std.in>
2023-01-16 10:08:41 -0600

    More license tweaks
    - Fix LICENSE file per directions at
      https://choosealicense.com/licenses/apache-2.0/ in an attempt
      to get GitHub to auto-detect it
    - Update copyright date in source files

--- src/state.js
+++ src/state.js
@@ -1,4 +1,4 @@
-// Copyright 2022 Peter Griess
+// Copyright 2023 Peter Griess
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
